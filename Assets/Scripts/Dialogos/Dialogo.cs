e985e1081eb8b3a03e08a0e08d2b0e5677165b81
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-12-21 18:09:27 -0300

    update : mudando tipo do atributo do id para int

--- Assets/Scripts/Dialogos/Dialogo.cs
+++ Assets/Scripts/Dialogos/Dialogo.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using Assets.Scripts.Dialogos.Enum;
+using Dialogos.Enum;
 using UnityEngine;
 
-namespace Assets.Scripts.Dialogos
+namespace Dialogos
 {
     [Serializable]
     public class Dialogo
     {
-        public string id;
+        public int id;
         public string orador;
         [TextArea(2, 10)]
         public string texto;
         public TipoDialogoEnum TipoDialogoEnum;
     }
-    
-
 }
