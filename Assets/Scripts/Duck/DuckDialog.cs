84203d24a567dc3e3811b914627c210f28cf06c7
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-12-21 18:08:25 -0300

    feat : exibindo acessorio do pato quando capturado

--- Assets/Scripts/Duck/DuckDialog.cs
+++ Assets/Scripts/Duck/DuckDialog.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections;
-using Assets.Scripts.Dialogos;
+using Assets.Scripts.Dialogos.Modal;
 using Dialogos;
 using UnityEngine;
 
@@ -7,23 +7,43 @@ namespace Duck
 {
     public class DuckDialog : DuckBehavior
     {
-        [SerializeField] private DuckDialogBase dialogoBase;
+        [SerializeField] private DialogoBase dialogoBase;
         [SerializeField] private DialogoObject dialogObject;
         [SerializeField] public GameObject iconeInteracao;
         [SerializeField] public GameObject acessorio;
         
         protected override IEnumerator Start()
         {
+            OnDuckRescued += AcessoriosPegos;
+            
             yield return base.Start();
             dialogoBase.SetDialogoObject(dialogObject);
         }
-        
+
+        private void OnDestroy()
+        {
+            OnDuckRescued += AcessoriosPegos;
+        }
+
         public override void OnPlayerInteraction()
         {
             if (IsFollowing) return;
             
             dialogoBase.StartDialogo();
         }
-        
+
+        void AcessoriosPegos()
+        {
+            if (IsRescued)
+            {
+                IsRescueded();
+            }
+        }
+
+        protected virtual void IsRescueded()
+        {
+            if (acessorio) acessorio.SetActive(true);
+            if (iconeInteracao) iconeInteracao.SetActive(false);
+        }
     }
 }
