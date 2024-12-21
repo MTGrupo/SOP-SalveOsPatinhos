81e87bb6ae990d15c178a4e1d505144f7c54dcf6
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-12-21 17:58:07 -0300

    feat : sub classe que lida com a logica do quiz do pato professor

---
+++ Assets/Scripts/Quiz/QuizTeacher.cs
@@ -0,0 +1,22 @@
+using Quiz;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Dialogos.Services
+{
+    public class QuizTeacher : QuestionManager
+    {
+        [FormerlySerializedAs("duck")] [SerializeField] private DialogDuckBase duckBase;
+        
+        public int MIN_ACERTOS = 3;
+        protected override void FinishedQuiz()
+        {
+            if (acertos >= MIN_ACERTOS)
+            {
+                base.FinishedQuiz();
+                duckBase.NextDialogo();
+                Debug.Log("Acertos: " + acertos);
+            }
+        }
+    }
+}
