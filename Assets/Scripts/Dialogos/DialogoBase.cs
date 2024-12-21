cbc92caa3cc2a06b31ad3748846029e48a255fbc
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-12-21 18:03:20 -0300

    update : mudando visibilidade de metodos

--- Assets/Scripts/Dialogos/DialogoBase.cs
+++ Assets/Scripts/Dialogos/DialogoBase.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using Dialogos;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,12 +12,11 @@ namespace Assets.Scripts.Dialogos.Modal
         [SerializeField] private TextMeshProUGUI orador;
         [SerializeField] private TextMeshProUGUI texto;
         [SerializeField] protected Button botaoProximo;
-        [SerializeField] private Button botaoFechar;
+        [SerializeField] protected Button botaoFechar;
         [SerializeField] protected Button zonaDeFechar;
 
-        protected DialogoObject dialogoObject;
+        public DialogoObject dialogoObject;
         protected int index = 0;
-        private bool dialogoAtivo = false;
         
         protected virtual void Start()
         {
@@ -37,8 +37,6 @@ namespace Assets.Scripts.Dialogos.Modal
         
         public void NextDialogo()
         {
-            if (!dialogoAtivo) return;
-            
             if (index < dialogoObject.dialogos.Count - 1)
             {
                 index++;
@@ -52,28 +50,24 @@ namespace Assets.Scripts.Dialogos.Modal
 
         public void NextDialogoNotShow()
         {
-            Debug.Log("NextDialogoNotShow");
             index++;
         }
         
-        public virtual void ShowDialogo()
+        protected virtual void ShowDialogo()
         {
             dialogoPainel.SetActive(true);
             orador.text = dialogoObject.GetDialogoAt(index).orador;
             texto.text = dialogoObject.GetDialogoAt(index).texto;
-            dialogoAtivo = true;
         }
 
         protected void OcultarDialogo()
         {
             dialogoPainel.SetActive(false);
-            dialogoAtivo = false;
         }
-        
+
         protected virtual void FinishedDialogo()
         {
             dialogoPainel.SetActive(false);
-            dialogoAtivo = false;
             index = 0;
         }
         
