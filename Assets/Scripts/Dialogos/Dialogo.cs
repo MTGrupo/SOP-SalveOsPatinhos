e10d2c6ccf72b7a419573a2418e1b8f179c61e13
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-01-02 13:08:36 -0300

    feat : novo atributo que identifica quem é o orador

--- Assets/Scripts/Dialogos/Dialogo.cs
+++ Assets/Scripts/Dialogos/Dialogo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Dialogos.Enum;
+using Mono.Cecil;
 using UnityEngine;
 
 namespace Dialogos
@@ -9,6 +10,7 @@ namespace Dialogos
     {
         public int id;
         public string orador;
+        public TypeSpeaker typeSpeaker;
         [TextArea(3, 10)]
         public string texto;
         public TipoDialogoEnum TipoDialogoEnum;
