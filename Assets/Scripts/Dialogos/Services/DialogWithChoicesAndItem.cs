b42b5399cfe174dcee47b62e9fa53675b1955988
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-01-08 15:58:30 -0300

    feat : sub classe para dialogos com tomada de decisão e que tenha um item

---
+++ Assets/Scripts/Dialogos/Services/DialogWithChoicesAndItem.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using Dialogos.Enum;
+using Dialogos.ObjectsOfDialogos;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dialogos.Services
+{
+    public class DialogWithChoicesAndItem : DialogDuckBase
+    {
+        [SerializeField] private Button btnSim;
+        [SerializeField] private Button btnNao;
+        [SerializeField] private ObjectBase objectBase;
+        [SerializeField] private GameObject mensagemItem;
+        
+        private bool isShowMensagemItem;
+        
+        protected override void Start()
+        {
+            base.Start();
+            
+            btnSim.gameObject.SetActive(false);
+            btnNao.gameObject.SetActive(false);
+            mensagemItem.SetActive(false);
+        }
+
+        protected override void ShowDialogo()
+        {
+            base.ShowDialogo();
+            
+            bool isDecision = dialogoObject.GetDialogoAt(index).TipoDialogoEnum == TipoDialogoEnum.Decisao;
+            btnSim.gameObject.SetActive(isDecision);
+            btnNao.gameObject.SetActive(isDecision);
+            botaoProximo.gameObject.SetActive(!isDecision);
+            
+            if (dialogoObject.GetDialogoAt(index).TipoDialogoEnum == TipoDialogoEnum.Buscando_Itens)
+            {
+                objectBase.gameObject.SetActive(true);
+                botaoProximo.gameObject.SetActive(false);
+                isShowMensagemItem = true;
+            }
+        }
+
+        protected override void ListenToEvents()
+        {
+            base.ListenToEvents();
+            btnSim.onClick.AddListener(EscolherSim);
+            btnNao.onClick.AddListener(EscolherNao);
+        }
+
+        void EscolherSim()
+        {
+            NextDialogo(); 
+        }
+
+        void EscolherNao()
+        {
+            OcultarDialogo();
+        }
+
+        protected override void OnDialogFechado()
+        {
+            base.OnDialogFechado();
+
+            if (isShowMensagemItem)
+            {
+                mensagemItem.SetActive(true);
+
+                StartCoroutine(HideMensagemItemAfterDelay(4.5f));
+            }
+        }
+
+        IEnumerator HideMensagemItemAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            mensagemItem.SetActive(false);
+            isShowMensagemItem = false;
+        }
+    }
+}
