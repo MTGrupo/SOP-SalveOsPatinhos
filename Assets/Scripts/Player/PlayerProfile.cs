8f7ebaff54aa5c21a10108fc99ccfd14a567fbeb
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-01-14 20:48:22 -0300

    update: adaptando a classe PlayerProfile para funcionar com o nome de jogador da conta autenticada

--- Assets/Scripts/Player/PlayerProfile.cs
+++ Assets/Scripts/Player/PlayerProfile.cs
@@ -1,20 +1,57 @@
+using System;
+using PlayerAccount;
 using UnityEngine;
 
 public class PlayerProfile : MonoBehaviour
 {
     public static PlayerProfile Instance { get; private set; }
-    public string PlayerName { get; private set; }
+    public event Action<string> OnPlayerNameChanged; 
+
+    private string playerName;
+    
+    public string PlayerName
+    {
+        get => playerName;
+        private set
+        {
+            playerName = value;
+            
+            OnPlayerNameChanged?.Invoke(value);
+        }
+    }
     
     public void SetPlayerName(string name)
     {
-        PlayerName = name;
+        if (!AuthenticationManager.Instance.IsAuthenticated)
+        {
+            PlayerName = name;
+            return;
+        }
+
+        AuthenticationManager.Instance.UpdatePlayerName(name);
+        PlayerName = AuthenticationManager.Instance.GetPlayerName().ToString();
     }
+
+    void ImportUnityPlayerName(bool isSignedIn)
+    {
+        if (!isSignedIn) return;
+
+        var playerName = AuthenticationManager.Instance.GetPlayerName();
+        
+        SetPlayerName(playerName.ToString());
+    }
+
+
+    private void Start()
+    {
+        AuthenticationManager.Instance.OnAuthenticationStateChanged += ImportUnityPlayerName;
+    }
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
