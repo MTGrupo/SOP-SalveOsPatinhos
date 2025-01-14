959dd8fb36d67ee53c39653e0799f3650a84daf0
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-01-14 20:39:34 -0300

    feat: criando uma classe responsável pela autenticação dos jogadores

---
+++ Assets/Scripts/PlayerAccount/AuthenticationManager.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Authentication.PlayerAccounts;
+using Unity.Services.Core;
+using UnityEngine;
+
+namespace PlayerAccount
+{
+    public class AuthenticationManager : MonoBehaviour
+    {
+        public static AuthenticationManager Instance { get; private set; }
+        
+        public bool IsAuthenticated => AuthenticationService.Instance.IsSignedIn;
+        
+        public event Action<bool> OnAuthenticationStateChanged;
+        
+        void Start()
+        {
+            OnAuthenticationStateChanged?.Invoke(IsAuthenticated);
+        }
+        
+        async Task InitializeServicesAsync()
+        {
+            try
+            {
+                await UnityServices.InitializeAsync();
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+        
+        async void SignedIn()
+        {
+            try
+            {
+                var accessToken = PlayerAccountService.Instance.AccessToken;
+                await SignInWithUnityAsync(accessToken);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                throw;
+            }
+        }
+    
+        async Task SignInWithUnityAsync(string accessToken)
+        {
+            try
+            {
+                await Task.Delay(100);
+                await AuthenticationService.Instance.SignInWithUnityAsync(accessToken);
+                Debug.Log("SignIn is successful.");
+                
+                OnAuthenticationStateChanged?.Invoke(IsAuthenticated);
+            }
+            catch (AuthenticationException ex)
+            {
+                Debug.LogException(ex);
+            }
+            catch (RequestFailedException ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        public async Task InitSignIn()
+        {
+            if (IsAuthenticated) return;
+            
+            try
+            {
+                await PlayerAccountService.Instance.StartSignInAsync();
+            }
+            catch (RequestFailedException ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+        
+        public void SignOut()
+        {
+            if (!IsAuthenticated) return;
+            
+            try
+            {
+                AuthenticationService.Instance.SignOut();
+                PlayerAccountService.Instance.SignOut();
+                AuthenticationService.Instance.ClearSessionToken();
+                
+                OnAuthenticationStateChanged?.Invoke(IsAuthenticated);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Unexpected error during logout: {e.Message}");
+            }
+        }
+
+        public async Task UpdatePlayerName(string name)
+        {
+            if (!IsAuthenticated) return;
+            
+            try
+            {
+                await AuthenticationService.Instance.UpdatePlayerNameAsync(name);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                throw;
+            }
+        }
+        
+        public async Task<string> GetPlayerName()
+        {
+            if (!IsAuthenticated) return null;
+            
+            try
+            {
+                return await AuthenticationService.Instance.GetPlayerNameAsync();
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                throw;
+            }
+        }
+        
+        async Task Reconnect()
+        {
+            try
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+            catch (RequestFailedException ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        void OnDestroy()
+        {
+            PlayerAccountService.Instance.SignedIn -= SignedIn;
+        }
+
+        async void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            
+            await InitializeServicesAsync();
+            
+            PlayerAccountService.Instance.SignedIn += SignedIn;
+            
+            if (!AuthenticationService.Instance.SessionTokenExists) return;
+            
+            await Reconnect();
+        }
+    }
+}
