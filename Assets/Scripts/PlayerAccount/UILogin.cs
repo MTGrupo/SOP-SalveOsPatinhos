d1688a7e825adc5b592a6bc17d3e61a930564e83
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-01-14 20:41:14 -0300

    feat: criando uma classe responsável pelo controle das UIs de login e logout

---
+++ Assets/Scripts/PlayerAccount/UILogin.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayerAccount
+{
+    public class UILogin : MonoBehaviour
+    {
+        [SerializeField] private Button loginButton;
+        [SerializeField] private Button logoutButton;
+        [SerializeField] private GameObject loginPainel;
+        [SerializeField] private Button cancelLoginButton;
+    
+        private void OnEnable()
+        {
+            loginButton.onClick.AddListener(OnLogin);
+            
+            if (!loginPainel)
+            {
+                AuthenticationManager.Instance.OnAuthenticationStateChanged += UpdateButtonVisibility;
+                logoutButton.onClick.AddListener(OnLogout);
+                UpdateButtonVisibility(AuthenticationManager.Instance.IsAuthenticated);
+                return;
+            }
+            
+            cancelLoginButton.onClick.AddListener(DisableLoginPainel);
+        }
+    
+        private async void OnLogin()
+        {
+            await AuthenticationManager.Instance.InitSignIn();
+        }
+
+        private void OnLogout()
+        {
+            AuthenticationManager.Instance.SignOut();
+        }
+        
+        private void UpdateButtonVisibility(bool isSignedIn)
+        {
+            if (!loginPainel)
+            {
+                loginButton.gameObject.SetActive(!isSignedIn);
+                logoutButton.gameObject.SetActive(isSignedIn);
+                return;
+            }
+
+            if (isSignedIn)
+            {
+                DisableLoginPainel();
+            }
+        }
+
+        private void DisableLoginPainel()
+        {
+            loginPainel.SetActive(false);
+        }
+
+        private void OnDisable()
+        {
+            AuthenticationManager.Instance.OnAuthenticationStateChanged -= UpdateButtonVisibility;
+            loginButton.onClick.RemoveListener(OnLogin);
+            
+            if (!loginPainel)
+            {
+                logoutButton.onClick.RemoveListener(OnLogout);
+                return;
+            }
+            
+            cancelLoginButton.onClick.RemoveListener(DisableLoginPainel);
+        }
+    }
+}
