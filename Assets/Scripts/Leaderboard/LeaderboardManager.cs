736f036860c13725249d1bd1c5e8071eeaae5d9c
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-01-14 20:42:47 -0300

    feat: criando uma classe responsável pelo gerenciamento da leaderboard

---
+++ Assets/Scripts/Leaderboard/LeaderboardManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using PlayerAccount;
+using Unity.Services.Leaderboards;
+using UnityEngine;
+
+namespace Leaderboard
+{
+    public class LeaderboardManager : MonoBehaviour
+    {
+        public static LeaderboardManager Instance { get; private set; }
+
+        public async Task SubmitScore(int score)
+        {
+            if (!AuthenticationManager.Instance.IsAuthenticated) return;
+            
+            try
+            {
+                await LeaderboardsService.Instance.AddPlayerScoreAsync("patos_resgatados", score);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+}
