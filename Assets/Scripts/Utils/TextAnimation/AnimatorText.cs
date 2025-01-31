27a7758754814f8aa3569c054219a97d85cdd87d
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-01-30 23:06:09 -0300

    feat : animação de texto, exibindo letra por letra

---
+++ Assets/Scripts/Utils/TextAnimation/AnimatorText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace.Utils
+{
+    public static class AnimatorText
+    {
+        static float fastTypingSpeed = 0.0001f;
+        static float speedDefault = 0.05f;
+        static float typingSpeed = speedDefault;
+
+        public static IEnumerator StartAnimatorText(TextMeshProUGUI textMesh, string fullText, System.Action onFinished)
+        {
+            textMesh.text = "";
+            ClickControllerSpeed.IsAnimationActive = true;
+            
+            for (int i = 0; i <= fullText.Length; i++)
+            {
+                textMesh.text = fullText.Substring(0, i);
+                float speed = ClickControllerSpeed.IsFastForward ? fastTypingSpeed : typingSpeed;
+                yield return new WaitForSeconds(speed);
+            }
+
+            onFinished?.Invoke();
+            typingSpeed = speedDefault;
+        }
+    }
+}
