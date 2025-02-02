6bcfdd6899807c38eed8e2817285281db87952c9
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-02 12:48:59 -0300

    feat : adicionar opções configuraveis para animação do texto no inspector

---
+++ Assets/Scripts/Utils/TextAnimation/AnimationTextController.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Utils
+{ 
+    public class AnimationTextController : MonoBehaviour
+    {
+        [field: SerializeField] public TextMeshProUGUI text { get; private set; } 
+        
+        [SerializeField] private float speed = 0.09f;
+        [SerializeField] private int stepLetters = 1;
+        [SerializeField] private bool useClickedFastAnimation = false;
+        [SerializeField] private Alignment alignment = Alignment.Left;
+        
+        private string fullText;
+        private void Start()
+        {
+            fullText = text.text;
+            AnimatorText.AnimationSettings(speed: speed, step: stepLetters, align: alignment, useClicked: useClickedFastAnimation);
+            StartCoroutine(AnimatorText.StartAnimatorText(text, fullText));
+        }
+        
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            if (text == null)
+            {
+                text = GetComponentInChildren<TextMeshProUGUI>();
+            }
+                
+        }
+#endif
+    }
+}
