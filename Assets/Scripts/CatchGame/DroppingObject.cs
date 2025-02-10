f5f90a0297b740c3e78f4f91e2a288650a5a2cd3
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-02-10 07:07:53 -0300

    feat: criando a classe DroppingObject para os objetos a serem pegados

---
+++ Assets/Scripts/CatchGame/DroppingObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CatchGame
+{
+    public class DroppableObject : MonoBehaviour
+    {
+        [SerializeField] int score;
+        public static event Action<int> OnCautch;
+        
+        [SerializeField] SpriteRenderer sprite;
+        [SerializeField] bool rotateSprite;
+        
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag("Catcher")) return;
+            
+            OnCautch?.Invoke(score);
+            Destroy();
+        }
+        
+        void RotateSprite()
+        {
+            if (!rotateSprite) return;
+            
+            var randomRotation = Random.Range(0, 360);
+            sprite.transform.rotation = Quaternion.Euler(0, 0, randomRotation);
+        }
+        
+        void Destroy()
+        {
+            Destroy(gameObject);
+        }
+        
+        void OnDestroy()
+        {
+            CGTimer.OnTimeOver -= Destroy;
+        }
+
+        void Start()
+        {
+            CGTimer.OnTimeOver += Destroy;
+            RotateSprite();
+        }
+    }
+}
