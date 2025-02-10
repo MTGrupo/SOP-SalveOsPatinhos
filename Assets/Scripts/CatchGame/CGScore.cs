78f8de201332d60e3d497a12d0ea92e658f14537
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-02-10 07:12:24 -0300

    feat: criando uma classe para a pontuação do Catch Game

---
+++ Assets/Scripts/CatchGame/CGScore.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CatchGame
+{
+    public class CGScore : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI scoreText;
+        int totalScore;
+
+        public int TotalScore
+        {
+            get => totalScore;
+            private set
+            {
+                totalScore = value;
+                
+                scoreText.text = totalScore.ToString();
+            }
+        }
+        
+        private void AddScore(int score)
+        {
+            totalScore += score;
+            scoreText.text = totalScore.ToString();
+        }
+        
+        private void OnDestroy()
+        {
+            DroppableObject.OnCautch -= AddScore;
+        }
+
+        private void Start()
+        {
+            DroppableObject.OnCautch += AddScore;
+        }
+    }
+}
