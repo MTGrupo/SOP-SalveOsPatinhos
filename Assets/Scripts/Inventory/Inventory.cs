4cc072adc6f732305efee66beb428e12be8f6021
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-10 17:50:16 -0300

    feat : abrindo e fechando inventario

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace.Inventory
+{
+    public class Inventory : MonoBehaviour
+    {
+        [SerializeField] private GameObject inventoryPanel;
+        [SerializeField] private Button buttonInventory;
+        
+        private Color defaultColor;
+        
+        private void Start()
+        {
+            inventoryPanel.SetActive(false);
+            defaultColor = buttonInventory.image.color;
+            buttonInventory.onClick.AddListener(ToggleInventory);
+        }
+
+        private void ToggleInventory()
+        {
+            bool isActive = inventoryPanel.activeSelf;
+            inventoryPanel.SetActive(!isActive);
+            buttonInventory.image.color = isActive ? defaultColor : defaultColor;
+        }
+        
+    }
+}
