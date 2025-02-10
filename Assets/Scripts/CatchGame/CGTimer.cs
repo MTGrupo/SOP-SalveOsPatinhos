03939142f92ad2d96e0799e1ed78cccb0c5e30aa
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-02-10 07:09:40 -0300

    feat: criando uma classe para o temporizador do Catch Game

---
+++ Assets/Scripts/CatchGame/CGTimer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace CatchGame
+{
+    public class CGTimer : MonoBehaviour
+    {
+        public static event Action OnTimeOver;
+        
+        TextMeshProUGUI timerText;
+        
+        float currentTime = 30;
+        bool isPaused = true;
+
+        private void Update()
+        {
+            if (isPaused) return;
+            
+            if(currentTime <= 0)
+            {
+                isPaused = true;
+                OnTimeOver?.Invoke();
+                return;
+            }
+            
+            currentTime -= Time.deltaTime;
+            UpdateTimerUI();
+        }
+        
+        void UpdateTimerUI()
+        {
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+            timerText.text = $"{seconds:00}";
+        }
+        
+        void SetPause(bool pause)
+        {
+            isPaused = pause;
+        }
+        
+        void StartTimer()
+        {
+            isPaused = false;
+        }
+        
+        void OnDestroy()
+        {
+            GameManager.OnGamePaused -= SetPause;
+            CatchGame.OnGameStarted -= StartTimer;
+        }
+
+        void Start()
+        {
+            GameManager.OnGamePaused += SetPause;
+            CatchGame.OnGameStarted += StartTimer;
+        }
+    }
+}
