a39a78c2ae4417f6eada2154f555064c58a4e6f3
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-11 18:27:48 -0300

    feat : agora quando o login é feito e caso tenha algo no inventario é pego

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace DefaultNamespace.Inventory
@@ -7,6 +10,7 @@ namespace DefaultNamespace.Inventory
     {
         [SerializeField] private GameObject inventoryPanel;
         [SerializeField] private Button buttonInventory;
+        [SerializeField] private List<Slot> slots = new();
         
         private Color defaultColor;
         
@@ -15,14 +19,59 @@ namespace DefaultNamespace.Inventory
             inventoryPanel.SetActive(false);
             defaultColor = buttonInventory.image.color;
             buttonInventory.onClick.AddListener(ToggleInventory);
+            LoadAndUpdateSlots();
         }
 
+        private void LoadAndUpdateSlots()
+        {
+            List<SlotManager.SlotData> loadedData = SlotManager.LoadSlotData();
+    
+            foreach (var slotData in loadedData)
+            {
+                Slot slot = slots.FirstOrDefault(s => s.name == slotData.slotName);
+                if (slot != null)
+                {
+                    slot.AddItemToSlot(slotData.objectName.ToLower() + ".png");
+                    slot.amountValue = slotData.amount;
+                }
+            }
+        }
+
+        
         private void ToggleInventory()
         {
             bool isActive = inventoryPanel.activeSelf;
             inventoryPanel.SetActive(!isActive);
             buttonInventory.image.color = isActive ? defaultColor : defaultColor;
         }
+        
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            InitializeSlots();
+        }
+        
+        private void InitializeSlots()
+        {
+            slots.Clear();
+            slots.AddRange(FindObjectsOfType<Slot>());
+            slots.Sort(CompareSlotNames);
+        }
+        
+        private int CompareSlotNames(Slot a, Slot b)
+        {
+            int nA = ExtractNumber(a.name);
+            int nB = ExtractNumber(b.name);
+            return nA.CompareTo(nB);
+        }
+
+        private int ExtractNumber(string name)
+        {
+            string numString = Regex.Match(name, @"\d+").Value;
+            return int.TryParse(numString, out int number) ? number : 0;
+        }
+#endif
+        
         
     }
 }
