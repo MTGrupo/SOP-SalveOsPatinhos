57c53da0ebefc12fa6a43de390c30c6eae522770
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-11 18:24:08 -0300

    feat : caso o valor do amount seja 0 o slot é limpado

--- Assets/Scripts/Inventory/Slot.cs
+++ Assets/Scripts/Inventory/Slot.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,57 +8,44 @@ namespace DefaultNamespace.Inventory
     public class Slot : MonoBehaviour
     {
         [SerializeField] private GameObject slotPrefab;
-        [SerializeField] private TextMeshProUGUI nameIcon;
+        [SerializeField] public TextMeshProUGUI nameIcon;
         [SerializeField] private Image icon;
         [SerializeField] public TextMeshProUGUI amount;
-        private bool isSlotBusy;
-        private string itemName; 
-        private int itemAmount;
-        public readonly List<string> namesInSlots = new();
+        public bool isSlotBusy { get; private set; }
+        
+        private int _amount;
+        public int amountValue { 
+            get => _amount;
+            set
+            {
+                _amount = Mathf.Max(1, value);
+                amount.text = _amount > 0 ? _amount.ToString() : "";
+                if (_amount == 0) ClearSlot();
+            }
+        }
+        
+        private string itemFileName;
         
         private void Awake()
         {
-            SetSlotVisibility(false); 
-            itemAmount = 0; 
+            SetSlotVisibility(false);
+            isSlotBusy = false;
+            amountValue = 1;
         }
 
         public void AddItemToSlot(string fileName)
         {
-            if (string.IsNullOrEmpty(fileName)) return;
+            if (string.IsNullOrEmpty(fileName) || isSlotBusy) return;
             
             string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
-
             if (!File.Exists(filePath)) return;
             
-            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
-            
-            ProcessItem(fileNameWithoutExtension, filePath);
-        }
-
-        private void ProcessItem(string itemNameWithoutExtension, string filePath)
-        {
-            itemName = itemNameWithoutExtension;
-            itemAmount = 1;
-            amount.text = itemAmount.ToString();
-            nameIcon.text = itemNameWithoutExtension;
+            nameIcon.text = Path.GetFileNameWithoutExtension(fileName);
             LoadImage(filePath);
             SetSlotVisibility(true);
-            MarkSlotAsBusy();
+            isSlotBusy = true;
+            itemFileName = fileName;
         }
-
-        public void DisplaySlotData()
-        {
-            Debug.Log($"Slot: {nameIcon.text}, Item: {itemName}, Quantity: {itemAmount}");
-        }
-        
-        private void MarkSlotAsBusy()
-        {
-            if (!isSlotBusy)
-            {
-                isSlotBusy = true;
-            }
-        }
-
         private void LoadImage(string path)
         {
             byte[] fileData = File.ReadAllBytes(path);
@@ -70,21 +56,16 @@ namespace DefaultNamespace.Inventory
             }
         }
         
-        public void RemoveItem()
+        private void ClearSlot()
         {
-            isSlotBusy = false;
             nameIcon.text = "";
             icon.sprite = null;
-            itemName = "";  
-            itemAmount = 0;  
+            isSlotBusy = false;
             SetSlotVisibility(false);
         }
         
+        public bool ContainsItem(string itemFileName) => itemFileName == this.itemFileName;
         private void SetSlotVisibility(bool isVisible) => slotPrefab.SetActive(isVisible);
-        public bool IsSlotBusy() => isSlotBusy;
-        public int GetAmount() => itemAmount;
-        public void IncrecementAmount() => itemAmount += 1;
-        
 
 #if UNITY_EDITOR
         private void Reset()
