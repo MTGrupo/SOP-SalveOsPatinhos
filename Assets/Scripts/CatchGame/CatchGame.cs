d923a6aa52904ffb61f2b289d208c09419a8679c
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-02-12 01:40:15 -0300

    feat: script para controlar o minigame

---
+++ Assets/Scripts/CatchGame/CatchGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace CatchGame
+{
+    public class CatchGame : MonoBehaviour
+    {
+        [field: SerializeField] public Collider Limit { get; private set; }
+        
+        public static CatchGame Instance { get; private set; }
+
+        public static event Action OnGameStarted;
+        
+        public static bool isFinished = false;
+        public static int duckAmount;
+        
+        
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            OnGameStarted?.Invoke();
+        }
+    }
+}
