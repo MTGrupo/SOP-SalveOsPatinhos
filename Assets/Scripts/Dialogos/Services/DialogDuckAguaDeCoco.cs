0d089935694e6c25c8fc15ec360ee3b0a75e8bfb
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-11 22:17:59 -0300

    feat : quando o dialogo é finalizado a quantidade de cocos são atualizados

--- Assets/Scripts/Dialogos/Services/DialogDuckAguaDeCoco.cs
+++ Assets/Scripts/Dialogos/Services/DialogDuckAguaDeCoco.cs
@@ -1,13 +1,15 @@
+using DefaultNamespace.Inventory;
 using Dialogos.Enum;
 using UnityEngine;
 
 namespace Dialogos.Services
 {
     public class DialogDuckAguaDeCoco : DialogDuckBase
-    {
+    { 
         public static DialogDuckAguaDeCoco Instance { get; private set; }
         
         public bool isDialogoCoco = false;
+        
         public void ShowMensagemCoco()
         {
             NextDialogoNotShow();
@@ -18,14 +20,40 @@ namespace Dialogos.Services
             
             if (dialogoObject.GetDialogoAt(index).TipoDialogoEnum == TipoDialogoEnum.Buscando_Itens)
             {
-                // zoneCloseDialogue.gameObject.SetActive(true);
+
+                if (SlotManager.GetSlotDataByName("coco")?.amount >= 3)
+                {
+                    dialogoObject.GetDialogoAt(index).texto = "Tome aqui pato, seus 3 cocos.";
+                    base.ShowDialogo();
+                    nextButton.gameObject.SetActive(true);
+                    zoneCloseDialogue.gameObject.SetActive(false);
+                    DeliverCoco();
+                    return;
+                }
+
+                dialogoObject.GetDialogoAt(index).texto = "Claro Pato, irei pegar os cocos no coqueiro.";
+                base.ShowDialogo();
                 nextButton.gameObject.SetActive(false);
                 isDialogoCoco = true;
+                DeliverCoco();
+                
                 return;
             }
             
             zoneCloseDialogue.gameObject.SetActive(false);
             nextButton.gameObject.SetActive(true);
+        }
+        
+        private void DeliverCoco()
+        {
+            if (SlotManager.RemoveItemFromInventory("coco", 3))
+            {
+                isDialogoCoco = false;
+            }
+            else
+            {
+                Debug.Log("Não há cocos sufucientes para entregar");
+            }
         }
         
         private void Awake()
