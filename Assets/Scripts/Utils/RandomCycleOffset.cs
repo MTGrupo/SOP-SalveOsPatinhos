aa58a81cacc2073edbd86d73fd7742c7d141940a
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-02-12 01:34:54 -0300

    feat: script para randomizar o frame inicial de animações

---
+++ Assets/Scripts/Utils/RandomCycleOffset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class RandomCycleOffset : StateMachineBehaviour
+    {
+        [SerializeField] float minOffset;
+        [SerializeField] float maxOffset;
+        
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            animator.SetFloat("cycleOffset", Random.Range(minOffset, maxOffset));
+        }
+    }
+}
