2b3757ab5de570a0ce5ab540406e57f698ba4680
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-14 20:07:04 -0300

    feat : classe que tem somente item, não contem decisão neça

---
+++ Assets/Scripts/Dialogos/Services/utilitarios/DialogWithItem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using Dialogos.Enum;
+using Dialogos.ObjectsOfDialogos;
+using UnityEngine;
+
+namespace Dialogos.Services
+{
+    public class DialogWithItem : DialogDuckBase
+    {
+        [SerializeField] private ObjectBase objectBase;
+        [SerializeField] private GameObject mensagemItem;
+        
+        private bool isShowMensagemItem;
+        
+        protected override void Start()
+        {
+            base.Start();
+            mensagemItem.SetActive(false);
+        }
+
+        protected override void ShowDialogo()
+        {
+            base.ShowDialogo();
+            
+            if (dialogoObject.GetDialogoAt(index).TipoDialogoEnum == TipoDialogoEnum.Buscando_Itens)
+            {
+                objectBase.gameObject.SetActive(true);
+                nextButton.gameObject.SetActive(false);
+                isShowMensagemItem = true;
+            }
+        }
+
+        protected override void OnDialogFechado()
+        {
+            base.OnDialogFechado();
+            
+            if (isShowMensagemItem)
+            {
+                mensagemItem.SetActive(true);
+                StartCoroutine(HideMensagemItemAfterDelay(4.5f));
+            }
+        }
+
+        IEnumerator HideMensagemItemAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            mensagemItem.SetActive(false);
+            isShowMensagemItem = false;
+        }
+    }
+}
