09a0ab97b0bdde92a1ab349d22c475c413394454
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-14 20:08:33 -0300

    feat : classe com metodo estatico que Dropa item ao entregar para o pato

---
+++ Assets/Scripts/Inventory/DropItem.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace.Inventory
+{
+    public class DropItem
+    {
+        public static bool Drop(string name, int quantity)
+        {
+            if (SlotManager.RemoveItemFromInventory(name, quantity))
+            {
+                SlotManager.LoadSlotData();
+                return true;
+            } 
+            
+            return false;
+        }
+    }
+}
