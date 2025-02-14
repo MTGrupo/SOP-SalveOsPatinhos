afe2e5597a56cda4b96bd7a3747b177567af4de1
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-02-14 00:15:23 -0300

    refactor: mudança no formato de exibição do texto do temporizador

--- Assets/Scripts/CatchGame/CGTimer.cs
+++ Assets/Scripts/CatchGame/CGTimer.cs
@@ -17,7 +17,7 @@ namespace CatchGame
         {
             if (isPaused) return;
             
-            if(currentTime <= 0)
+            if(currentTime < 1)
             {
                 isPaused = true;
                 OnTimeOver?.Invoke();
@@ -31,7 +31,7 @@ namespace CatchGame
         void UpdateTimerUI()
         {
             int seconds = Mathf.FloorToInt(currentTime % 60);
-            timerText.text = $"{seconds:00}";
+            timerText.text = $"{seconds:0}";
         }
         
         void SetPause(bool pause)
