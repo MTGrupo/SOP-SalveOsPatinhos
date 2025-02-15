630509990ae881c75035ed5d7eb28fc0d086d136
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-15 08:57:43 -0300

    feat : interface para condições de dialogos baseadas em itens

---
+++ Assets/Scripts/Dialogos/Services/utilitarios/Interfaces/IDialogItemChecker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Dialogos.Services.utilitarios.Interfaces
+{
+    public interface IDialogItemChecker
+    {
+        void AddCondition(int dialogId, string itemName, int requiredAmount, Action action);
+        bool CheckAndExecute(int dialogId);
+    }
+}
