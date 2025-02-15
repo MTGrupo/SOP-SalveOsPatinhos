7cc04eb4884cf8a3d992f4206e5bcb1e8188ddb0
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-15 09:02:06 -0300

    refactor : utilizando a classe "DialogItemChecker" para deixar o dialogo mais dinamico caso o jogador tenha 3 cocos no inventario

--- Assets/Scripts/Dialogos/Services/especificos/DialogDuckAguaDeCoco.cs
+++ Assets/Scripts/Dialogos/Services/especificos/DialogDuckAguaDeCoco.cs
@@ -1,11 +1,12 @@
 ﻿using DefaultNamespace.Inventory;
 using Dialogos.Enum;
-using UnityEngine;
+using Dialogos.Services.utilitarios.Interfaces;
 
 namespace Dialogos.Services
 {
     public class DialogDuckAguaDeCoco : DialogDuckBase
     { 
+        private IDialogItemChecker itemChecker = new DialogItemChecker();
         public static DialogDuckAguaDeCoco Instance { get; private set; }
         
         public bool isDialogoCoco = false;
@@ -14,32 +15,31 @@ namespace Dialogos.Services
         {
             NextDialogoNotShow();
         }
+
+        private void Start()
+        {
+            itemChecker.AddCondition(dialogId:2, itemName:"coco", requiredAmount: 3, () =>
+            {
+                dialogoObject.GetDialogoAt(index).texto = "Tome aqui pato, seus 3 cocos.";
+                nextButton.gameObject.SetActive(true);
+                zoneCloseDialogue.gameObject.SetActive(false);
+                DeliverCoco();
+            });
+            
+            base.Start();
+        }
         protected override void ShowDialogo()
         {
+            base.ShowDialogo();
+            if (itemChecker.CheckAndExecute(dialogoObject.GetDialogoAt(index).id)) return;
             base.ShowDialogo();
             
             if (dialogoObject.GetDialogoAt(index).TipoDialogoEnum == TipoDialogoEnum.Buscando_Itens)
             {
-
-                if (SlotManager.GetSlotDataByName("coco")?.amount >= 3)
-                {
-                    dialogoObject.GetDialogoAt(index).texto = "Tome aqui pato, seus 3 cocos.";
-                    base.ShowDialogo();
-                    nextButton.gameObject.SetActive(true);
-                    zoneCloseDialogue.gameObject.SetActive(false);
-                    DeliverCoco();
-                    return;
-                }
-
                 dialogoObject.GetDialogoAt(index).texto = "Claro Pato, irei pegar os cocos no coqueiro.";
-                base.ShowDialogo();
                 nextButton.gameObject.SetActive(false);
                 isDialogoCoco = true;
-                return;
             }
-            
-            zoneCloseDialogue.gameObject.SetActive(false);
-            nextButton.gameObject.SetActive(true);
         }
 
         protected override void FinishedDialogo()
@@ -50,14 +50,9 @@ namespace Dialogos.Services
 
         private void DeliverCoco()
         {
-            if (SlotManager.RemoveItemFromInventory("coco", 3))
+            if (DropItem.Drop("coco", 3))
             {
-                SlotManager.LoadSlotData();
                 isDialogoCoco = false;
-            }
-            else
-            {
-                Debug.Log("Não há cocos sufucientes para entregar");
             }
         }
         
