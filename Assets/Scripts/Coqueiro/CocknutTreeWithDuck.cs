42ffe065547ca5273b750b7c6b83e0fd1d964006
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-02-15 17:35:17 -0300

    feat: dialogo do pato preso no coqueiro e conexão com o catch game

---
+++ Assets/Scripts/Coqueiro/CocknutTreeWithDuck.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Dialogos.Services.especificos;
+using Duck;
+using Interaction;
+using MiniGame;
+using UnityEngine;
+
+namespace Coqueiro
+{
+    public class CocknutTreeWithDuck : InteractableObject, IInteraction
+    {
+        [SerializeField] private DialogDuckOnTree dialog;
+        [SerializeField] private List<DuckBehavior> ducks;
+        [SerializeField] private Collider2D colisor;
+        [SerializeField] private GameObject iconeInteracao;
+        [SerializeField] private int miniGameID;
+        
+        public void OnPlayerInteraction()
+        {
+            MiniGameSession.currentMiniGameID = miniGameID;
+            GameManager.SaveGameData();
+            dialog.StartDialogo();
+        }
+
+        public void EnableDuck(int index)
+        {
+            ducks[index].gameObject.SetActive(true);
+            ducks[index].StartFollowing();
+            GameManager.SaveGameData();
+        }
+        
+        void OnDestroy()
+        {
+            RemoveObject(colisor);
+        }
+        
+        private IEnumerator Start()
+        {
+            // É um jogo novo? Se sim, parar processo.
+            if(!GameManager.IsLoadingGameData)
+            {
+                foreach (var duck in ducks)
+                {
+                    duck.gameObject.SetActive(false);
+                }
+                AddObject(colisor, this);
+                yield break;
+            }
+            
+            yield return new WaitWhile(() => GameManager.IsLoadingGameData);
+            
+            // Algum pato já foi resgatado? Se sim, desativar o ícone de interação e parar processo.
+            foreach (var duck in ducks)
+            {
+                if (!duck.IsRescued) continue;
+                iconeInteracao.SetActive(false);
+                yield break;
+            }
+            
+            foreach (var duck in ducks) duck.gameObject.SetActive(false);
+            
+            // O minigame já foi finalizado? se sim, soltar a quantidade de patos resgatados, desativar o ícone de interação e parar processo.
+            var catchGameResults = CatchGame.CatchGame.GetResult(miniGameID);
+            if (catchGameResults.isFinished)
+            {
+                for (int i = 0; i < catchGameResults.ducksRecued; i++)
+                {
+                    EnableDuck(i);
+                }
+                iconeInteracao.SetActive(false);
+                yield break;
+            }
+            
+            // Adicionar o colisor na lista de objetos interativos.
+            AddObject(colisor, this);
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogos/Services/especificos/DialogDuckOnTree.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Dialogos.Modal;
+using Dialogos.Enum;
+
+namespace Dialogos.Services.especificos
+{
+    public class DialogDuckOnTree : DialogoBase, IChangeScene
+    {
+        protected override void ShowDialogo()
+        {
+            base.ShowDialogo();
+            nextButton.gameObject.SetActive(true);
+            var tipoDialogo = dialogoObject.GetDialogoAt(index).TipoDialogoEnum;
+            HandleSceneChange(tipoDialogo);
+        }
+        
+        public void HandleSceneChange(TipoDialogoEnum tipoDialogoEnum)
+        {
+            if (tipoDialogoEnum == TipoDialogoEnum.ChangeScene)
+            {
+                dialoguePanel.SetActive(false);
+                GameManager.LoadCatchGame();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+            } 
+        }
+    }
+}
