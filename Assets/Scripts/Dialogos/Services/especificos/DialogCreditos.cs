214c8b5da07f1dafec28ff5e180a4733caf252c6
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-14 21:26:14 -0300

    feat : classe para o dialogo dos creditos

---
+++ Assets/Scripts/Dialogos/Services/especificos/DialogCreditos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using Assets.Scripts.Dialogos.Modal;
+using UnityEngine;
+
+namespace Dialogos.Services.especificos
+{
+    public class DialogCreditos : DialogoBase
+    {
+        protected override void ShowDialogo()
+        {
+            base.ShowDialogo();
+            StartCoroutine(CloseDialogo());
+        }
+
+        private IEnumerator CloseDialogo()
+        {
+            yield return new WaitForSeconds(3f);
+            FinishedDialogo();
+        }
+    }
+}
